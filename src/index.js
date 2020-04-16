c22d1743484bb0cfaf884749c89171bfcf7aeef3
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-16 14:54:41 -0300

    Configured Reactotron

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import './config/ReactotronConfig';
+
 import App from './App';
 
 export default function Index() {
