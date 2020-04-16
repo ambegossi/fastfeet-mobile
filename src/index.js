56ea773441ba731cf7c6c1e3e07c9121d1453730
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-16 17:10:20 -0300

    Configured StatusBar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -15,6 +16,7 @@ export default function Index() {
     <NavigationContainer>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
           <App />
         </PersistGate>
       </Provider>
